879e9fc66b7553a2f0c84556768dfeff0a6a3c74
Oscar <oscar.ramos@ucsp.edu.pe>
2020-10-19 10:53:57 -0500

    ✨ Setup boilerplate code

---
+++ src/globalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.onBackground};
+    transition: all 0.50s linear;
+  }
+`
