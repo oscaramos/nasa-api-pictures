0aaae80448b146072efcdefb281d320e046b9104
Oscar <oscar.ramos@ucsp.edu.pe>
2020-12-02 16:29:21 -0500

    ♻ Moved confirmation message to context api

---
+++ src/hooks/useConfirmation.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState, createContext, useContext } from 'react'
+import styled from 'styled-components'
+
+const SavedConfirmationContainer = styled.div`
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
+  border-radius: 8px;
+  
+  position: fixed;
+  padding: 16px;
+  bottom: 24px;
+  right: 24px;
+`
+
+function SavedConfirmation({ hidden, message }) {
+  if (hidden) {
+    return null
+  }
+
+  return (
+    <SavedConfirmationContainer>
+      <div style={{ fontSize: 36, fontWeight: 'bold' }}>
+        { message }
+      </div>
+    </SavedConfirmationContainer>
+  )
+}
+
+const SavedConfirmationContext = createContext(undefined)
+
+export function ConfirmationProvider({ children }) {
+  const [hidden, setHidden] = useState(true)
+  const [message, setMessage] = useState("")
+
+  const show = (message) => {
+    setHidden(false)
+    setMessage(message)
+    setTimeout(() => setHidden(true), 2000)
+  }
+
+  return (
+    <SavedConfirmationContext.Provider value={ { show } }>
+      { children }
+
+      <SavedConfirmation hidden={hidden} message={message} />
+    </SavedConfirmationContext.Provider>
+  )
+}
+
+export function useConfirmation() {
+  const context = useContext(SavedConfirmationContext)
+  if (context === undefined) {
+    throw new Error('useSavedConfirmation must be within a SavedConfirmationProvider')
+  }
+  return context
+}
