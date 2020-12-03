19deadc5b7ec57f90fcab758575fe6ed0ad6131c
Oscar <oscar.ramos@ucsp.edu.pe>
2020-12-02 22:31:08 -0500

    ✨ Added favorites logic to PictureData page

--- src/pages/PictureData.js
+++ src/pages/PictureData.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Clickable from '../components/Clickable'
 import { usePictureData } from '../hooks/usePictureData'
-import { useRoute } from '../hooks/useRoute'
+import { useFavorites } from '../hooks/useFavorites'
 
 const Container = styled.div`
   height: 100vh;
@@ -11,7 +11,7 @@ const Container = styled.div`
   align-items: center;
 `
 
-const CardContent = styled.div`
+const PictureContent = styled.div`
   padding: 16px;
 `
 
@@ -21,42 +21,53 @@ const Img = styled.img`
   margin: 0;
 `
 
-function PictureData() {
-  const [picture] = usePictureData()
-  const [, setRoute] = useRoute()
-
-  const { date, explanation, hdurl, title } = picture
-  if (picture === null) {
-    return null
-  }
-
-  const handleBackToHome = () => {
-    setRoute('/home')
-  }
-
+function PictureDataView({ title, explanation, date, hdurl, onAddFavorite, onRemoveFavorite, isAddedToFavorites}) {
   return (
     <Container>
-      <CardContent>
-        <Clickable style={{ fontSize: 18, fontWeight: 'normal' }} onClick={handleBackToHome}>
-          Go back
-        </Clickable>
+      <PictureContent>
         <h3 style={{ fontSize: 26, marginBottom: 16, marginTop: 50 }}>
           {title}
         </h3>
-        <Clickable style={{ fontSize: 18, fontWeight: 'normal' }}>
-          Add to favorites
-        </Clickable>
+        {
+          isAddedToFavorites
+            ? <Clickable style={{ fontSize: 18, fontWeight: 'normal' }} onClick={onRemoveFavorite}>
+                Remove from favorites
+              </Clickable>
+            : <Clickable style={{ fontSize: 18, fontWeight: 'normal' }} onClick={onAddFavorite}>
+               Add to favorites
+              </Clickable>
+        }
+
         <p style={{ fontSize: 18 }}>
           {explanation}
         </p>
         <h4>
           { date }
         </h4>
-      </CardContent>
+      </PictureContent>
       <a href={hdurl} target="_blank">
         <Img src={hdurl} />
       </a>
     </Container>
+  )
+}
+
+function PictureData() {
+  const [picture] = usePictureData()
+  const [, { add, remove, isIncluded }] = useFavorites()
+
+  const { date, explanation, hdurl, title } = picture
+
+  return (
+    <PictureDataView
+      title={title}
+      explanation={explanation}
+      date={date}
+      hdurl={hdurl}
+      onAddFavorite={() => add(picture)}
+      onRemoveFavorite={() => remove(picture)}
+      isAddedToFavorites={isIncluded(picture)}
+    />
   )
 }
 
